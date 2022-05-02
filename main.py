c492f78d233dd8d11f8fed0c9c0efcae495759f0
Ricardo Dematte <103331571+ricardodematte@users.noreply.github.com>
2022-05-02 19:36:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+'''
+Faça um algoritmo que peça o nome do aluno, código do aluno (prontuário),
+telefone e e-mail e mostre estas informações na tela
+'''
+
+import os
+nome = (input("Digite seu nome:\n"))
+os.system("clear")
+cod = (input("Digite seu código:\n"))
+os.system("clear")
+tel = (input("Digite seu telefone:\n"))
+os.system("clear")
+email = (input("Digite seu e-mail:\n"))
+os.system("clear")
+print ("Nome é ",nome)
+print ("Código é ",cod)
+print ("Telefone é ",tel)
+print ("E-mail é ",email)
